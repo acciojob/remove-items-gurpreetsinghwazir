120fa03c1d31a2cac539d2f0802b383aa13a1943
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-23 22:55:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+let btn = document.querySelector("input")
+let select = document.getElementById("colorSelect");
+
+//select.addEventListener("change", display);
+btn.addEventListener("click", display);
+
+//console.log(s[s.selectedIndex].value); // get value
+
+function display(eventDetails){
+let remo =(select.options[select.selectedIndex].tagName);
+
+
+
+}
+
+
+
+
+
