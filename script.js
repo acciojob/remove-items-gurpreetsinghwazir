8be2db59d787a1cb1f9921f5fc77804b7f30fb24
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-06-15 08:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 let btn = document.querySelector("input")
 let select = document.getElementById("colorSelect");
 
-//select.addEventListener("change", display);
 btn.addEventListener("click", display);
 
-//console.log(s[s.selectedIndex].value); // get value
-
 function display(eventDetails){
-let remo =(select.selectedIndex);
 
-if(remo){
-//console.log(remo)
-select.remove(remo);
-}
+
+
+
+select.remove(select.selectedIndex);
+
 
 
 
